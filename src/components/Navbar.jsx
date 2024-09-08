8f1b88cfc1f2f3dcce520edb6520635a3d87f74a
Ikkyuz <djdjv999@gmail.com>
2024-09-09 00:07:52 +0700

    Update commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import Logo from "../assets/Logo.png"
 
 const Navbar = () => {
   return (
-    <nav className='bg-gray-950 p-4  flex justify-between items-center fixed w-full top-0 z-50'>
+    <nav className='bg-gray-950  p-3 flex justify-between items-center fixed w-full top-0 z-50'>
       <div className='flex items-center ml-10'>
         {/* Logo */}
         <img src={Logo} alt="Logo" className='w-16 h-16 ml-10'/>
@@ -15,10 +15,13 @@ const Navbar = () => {
       </div>
 
       {/* Linkpage */}
-      <ul className='flex space-x-4 mr-20'>
-        <li><a href="" className='text-white text-xl hover:text-red-600'>HOME</a></li>
-        <li><a href="" className='text-white text-xl hover:text-red-600'>PRODUCT</a></li>
-        <li><a href="" className='text-white text-xl hover:text-red-600'>CONTACT</a></li>
+      <ul className='flex items-center justify-between space-x-4 mr-20'>
+        <li><a href="" className='text-white text-xl mr-5 hover:text-red-600'>HOME</a></li>
+        <li><a href="" className='text-white text-xl mr-5 hover:text-red-600'>PRODUCT</a></li>
+        <li><a href="" className='text-white text-xl mr-5 hover:text-red-600'>BLOG</a></li>
+        <li><a href="" className='text-white text-xl mr-5 hover:text-red-600'>CONTACT</a></li>
+        <li><a href="" className='text-white hover:text-red-600'><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-search"><circle cx="11" cy="11" r="8"/><path d="m21 21-4.3-4.3"/></svg></a></li>
+        <li><a href="" className='text-white hover:text-red-600'><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-menu"><line x1="4" x2="20" y1="12" y2="12"/><line x1="4" x2="20" y1="6" y2="6"/><line x1="4" x2="20" y1="18" y2="18"/></svg></a></li>
       </ul>
     </nav>
   )
