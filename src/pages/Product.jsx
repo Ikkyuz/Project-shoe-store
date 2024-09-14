272bc4265555cbe24c7fdd03eb9e01d5ced8dab1
Ikkyuz <djdjv999@gmail.com>
2024-09-14 14:42:09 +0700

    Update files

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -94,7 +94,7 @@ const Product = () => {
     <Layout>
       <div className="container items-center justify-center mx-auto py-8 mt-24">
         <h1 className='text-4xl font-bold text-center mb-3'>PRODUCT LIST</h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
           {products.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
